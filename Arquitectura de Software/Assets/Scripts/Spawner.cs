d1ef1465636392e9742b5786a416620b355b1d45
miguel angel <migel.a.l.z@gmail.com>
2021-01-31 21:20:13 +0100

    Creación del escenario de juego

--- Arquitectura de Software/Assets/Scripts/Spawner.cs
+++ Arquitectura de Software/Assets/Scripts/Spawner.cs
@@ -6,12 +6,14 @@ public class Spawner : MonoBehaviour
 {
     [Header("gameObjects")]
     public GameObject EnemigoPrefab;
-    public GameObject Spawn;
+    public GameObject SpawnEnemigo;
 
-    [Header("variables Spawn")]
+    [Header("variables Spawn De enemigos")]
     [SerializeField]private float tiempoSpawn =10f;
     [SerializeField]private float tiempo =0;
 
+    [Header("variables Spawn De monedas")]
+    public List<GameObject> listaSpawns;
     ObjectPooling objectPooler;
 
     // Start is called before the first frame update
@@ -20,22 +22,23 @@ public class Spawner : MonoBehaviour
         objectPooler = ObjectPooling.llamada;
 
     }
-
     // Update is called once per frame
     void Update()
     {
+
+        //Aqui ocurre el spawn de los enemigos ya generados en una pool.
         for(int i = 0;i < tiempoSpawn;i++)
         {          
           tiempo += 1*Time.deltaTime;
           if(tiempo >= tiempoSpawn)
-          {
-           //Instantiate(EnemigoPrefab).transform.position = Spawn.transform.position;
-           objectPooler.SpawnFromPool("Enemigo", Spawn.transform.position, Quaternion.identity);
+          {         
+           //esta linea es la que activa al enemigo del pool          
+           objectPooler.SpawnFromPool("Enemigo", SpawnEnemigo.transform.position, Quaternion.identity);
            tiempo = 0;
-          }
-          
+          }        
         }
-        
+        //Aqui ocurre el spawn de las monedas ya generados en una pool.
+        //objectPooler.SpawnFromPool("Coin", Spawn.transform.position, Quaternion.identity);
     }
 
     
